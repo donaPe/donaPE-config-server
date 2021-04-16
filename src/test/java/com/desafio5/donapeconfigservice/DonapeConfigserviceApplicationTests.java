8e83a44149e80e5181c2868ac1603f4fd1c6b264
montenegrojuarezc <montenegrojuarezc@gmail.com>
2021-04-16 10:18:44 -0500

    configserver

---
+++ src/test/java/com/desafio5/donapeconfigservice/DonapeConfigserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio5.donapeconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonapeConfigserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
